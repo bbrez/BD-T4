cb2ab05c48d27ab4d967f82e110f112115498df5
Bruno <bbrezolin@hotmail.com>
2021-03-13 14:11:05 -0300

    inicialização trabalho

---
+++ src/main/java/com/blv/trabbd4/TrabalhoBd4Application.java
@@ -0,0 +1,13 @@
+package com.blv.trabbd4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoBd4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrabalhoBd4Application.class, args);
+    }
+
+}
