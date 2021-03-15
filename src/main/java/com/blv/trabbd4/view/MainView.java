2b5355f4d21c2175b9f306bfa6265bc39ee16c38
Bruno <bbrezolin@hotmail.com>
2021-03-15 14:33:26 -0300

    Views

--- src/main/java/com/blv/trabbd4/view/MainView.java
+++ src/main/java/com/blv/trabbd4/view/MainView.java
@@ -1,5 +1,29 @@
 package com.blv.trabbd4.view;
 
+import java.util.Scanner;
+
 public class MainView {
-    public MainView(){}
+    private boolean running;
+
+    public MainView(){
+        this.running = true;
+
+        Scanner s = new Scanner(System.in);
+        while(this.running) {
+            int selected = s.nextInt();
+            switch (selected) {
+                case 1: //Cliente
+                    ClienteView cv = new ClienteView();
+                    break;
+                case 2: //Fatura
+                    FaturaView fv = new FaturaView();
+                    break;
+                case 0: //Sair
+                    this.running = false;
+                    break;
+
+            }
+        }
+
+    }
 }
